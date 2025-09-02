3d5698b75576e9d6eac4cc22de0174857ce0184c
samerrbarakat <barakatsamer932@gmail.com>
2025-09-02 21:56:36 +0300

    Attempting fixing CORS

--- src/handlers/experiments/create.ts
+++ src/handlers/experiments/create.ts
@@ -81,6 +81,15 @@ export const createExperimentHandler: APIGatewayProxyHandler = async (
 
   return {
     statusCode: 201,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type,Authorization",
+      "X-Content-Type-Options": "nosniff",
+      "X-Frame-Options": "DENY",
+      "Referrer-Policy": "no-referrer-when-downgrade",
+      "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+    },
     body: JSON.stringify({ experimentId: dbItem.experimentId }),
   };
 };
